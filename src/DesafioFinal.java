f2672c5b15dad6e286726a2e7fd43bc562b67647
Gabrielemail <gmedeiros144@gmail.com>
2024-09-14 23:37:39 -0300

    Segundo commit

--- src/DesafioFinal.java
+++ src/DesafioFinal.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class DesafioFinal {
+    public static void main(String[] args) {
+
+        String nome = "Gabriel Medeiros";
+        String tipoConta = "Corrente";
+        double saldo = 1100.10;
+
+        System.out.println("**************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("**************************");
+
+        String menu = """
+                **Digite sua opção**
+                1-Consultar saldo
+                2-Transferir valor
+                3-Receber valor
+                4-Sair
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+        int opcao =0;
+        while (opcao != 4){
+           System.out.println(menu);
+           opcao = leitura.nextInt();
+
+           if(opcao == 1){
+               System.out.println("O saldo atual é: " + saldo);
+           }else if(opcao == 2){
+               System.out.println("Qual o valor deseja transferir: ");
+               double valorTransferido = leitura.nextDouble();
+               if(valorTransferido > saldo){
+                   System.out.println("Não há saldo suficiente para fazer essa transferência");
+               } else{
+                   double saldoAtualizado = saldo - valorTransferido;
+                   System.out.println("Saldo atualizado R$ " + saldoAtualizado);
+               }
+           }else if(opcao == 3){
+                   System.out.println("Informe o valor a receber: ");
+                   double valorRecebido = leitura.nextDouble();
+                   double valorTotal = saldo + valorRecebido;
+                   System.out.println("Saldo atualizado: " + valorTotal);
+           } else if (opcao !=4){
+               System.out.println("Opção inválida");
+           }
+
+
         }
+
     }
 }
